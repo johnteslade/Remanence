bba9460ceed8fc609021f58695bc0d5423b37d4f
JohnTESlade <john@jtes.net>
2011-09-06 22:24:25 +0100

    Completing output byte array creation

--- clean_zeros.py
+++ clean_zeros.py
@@ -1,20 +1,38 @@
 import pprint
 
-ba = bytearray(b'\xff\x00\xff\x00\x00\xff\x00\x00\x00\x00\xff\xff\xff')
+# Returns byte array which runs of bytes removed
+def strip_byte_runs(in_ba, run_byte, run_threshold):
 
-zero_run = 0
-ZERO_RUN_THRESH = 2
+	zero_run = 0
 
-for i in range(0, len(ba)):
+	out_ba = bytearray()
+
+	for i in range(0, len(in_ba)):
+		
+		byte = in_ba[i]
+
+		# Look for runs
+		if byte == run_byte:
+			zero_run = zero_run + 1
+		# If end of run then add runs less that the threshold
+		else:
+			if zero_run > 0 and zero_run <= run_threshold:
+				for i in range(0, zero_run): out_ba.append(run_byte)
+			out_ba.append(byte)
+			zero_run = 0
+
+	return out_ba
+
+def main():
+
+	ba = bytearray(b'\xaa\x00\xbb\x00\x00\xcc\x00\x00\x00\xdd\x00\x00\x00\x00\xee\xff\xff')
+
+	out_ba = bytearray()
 	
-	byte = ba[i]
+	out_ba = strip_byte_runs(ba, 0, 2)
 
-	if byte == 0:
-		zero_run = zero_run + 1
-	else:
-		if zero_run > 0 and zero_run <= ZERO_RUN_THRESH:
-			print "add run of %d" % zero_run
-		print byte
-		zero_run = 0
+	pprint.pprint(ba)
+	pprint.pprint(out_ba)
 
-pprint.pprint(ba)
+main()
+
