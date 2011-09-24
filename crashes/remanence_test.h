c74deac55e58dceaf70367dbf670618b7a60aeaf
JohnTESlade <john@jtes.net>
2011-09-24 12:04:56 +0100

    Additional tasks

--- crashes/remanence_test.h
+++ crashes/remanence_test.h
@@ -6,5 +6,7 @@
 /* Macro to cause program to crash and core dump */
 #define REMANENCE_CRASH { int * temp_ptr = 0; int i; i = *temp_ptr; }
 
+// TODO define macros and the sensitive strings here - do not use static declarations here as it will show up in the core dump
+
 #endif /* remanence_test_h */
 
